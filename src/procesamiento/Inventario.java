a0aa60eec49e8fbcef6a392025aebde4ed37a4cf
Sydus1 <j.sierraa@uniandes.edu.co>
2021-10-11 19:11:51 -0500

    Clases y Metodos Completos
    Faltan las herencias

--- src/procesamiento/Inventario.java
+++ src/procesamiento/Inventario.java
@@ -1,5 +1,34 @@
 package procesamiento;
 
 public class Inventario {
-
+	public void getCostoProducto(String codigoBarras) {
+		
+	}
+	
+	public void comprar(String codigoBarras, int cantidad) {
+		
+	}
+	
+	public void registrarLote(String codigoBarras, int cantidadOriginal, int cantidadActual, String fecha_vencimiento,
+			int costoTotal, int precio_publico_unidad, int precio_unidad_medida, String nombre,
+			String categoria, String subcategoria1, String subcategoria2, String tipoRefrigerado, int precioActual) {
+		
+	}
+	
+	public void registrarProducto(int codigoBarras, int cantidadOriginal, int cantidadActual, String fecha_vencimiento,
+			int costoTotal, int precio_publico_unidad, int precio_unidad_medida) {
+		
+	}
+	
+	public void eliminarLote(String codigoBarras, String fecha) {
+		
+	}
+	
+	public void crearArchivos() {
+		
+	}
+	
+	public void actualizarArchivos() {
+		
+	}
 }
--- src/procesamiento/RegistroCompras.java
+++ src/procesamiento/RegistroCompras.java
@@ -1,5 +1,31 @@
 package procesamiento;
 
+import modelo.Compra;
+
+import java.util.Map;
+
+
+
 public class RegistroCompras {
 
+	public void registrarCompra(Map productosyCantidades, String nombre, String cedula) {
+		
+	}
+	
+	public void guadarCompra(Compra compra) {
+		
+	}
+	
+	public void crearArchivos() {
+		
+	}
+	
+	public void leerInfoArchivos() {
+		
+	}
+	
+	public void actualizarArchivos() {
+		
+	}
+	
 }
