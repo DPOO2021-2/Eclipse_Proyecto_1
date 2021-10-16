3d0c5134e3b5c5d4c9801f29480b6c0c0dcc60b4
Sydus1 <j.sierraa@uniandes.edu.co>
2021-10-16 15:27:42 -0500

    Arreglo Cliente

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -5,7 +5,6 @@ public class Cliente {
 	private String nombre;
 	private String cedula;
 	
-	public Cliente(String nombre, String cedula)
 	//estos puntos son de una sola compra ya que este no es un cliente registrado
 	private double puntos;
 	
